30610e2074ee72a9516b1bd6a4b656ba428c7362
maverick-amith <maverick.amith@gmail.com>
2025-08-08 23:56:03 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to FunLearn!</h1>
+      <p>Your Kannada and Hindi learning adventure starts here.</p>
+    </div>
+  );
+}
+
+export default App;
